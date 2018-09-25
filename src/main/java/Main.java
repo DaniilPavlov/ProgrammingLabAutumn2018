c661ea18ee5bd463ed0b87a3a7ab2b64cd5c96a0
DaniilPavlov <cool.sharagin@mail.ru>
2018-09-25 23:39:10 +0400

    better setLocation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,7 +9,7 @@ public class Main {
         JFrame frame = new JFrame("Reversi");
         frame.setContentPane(new Reversi());
         frame.setSize(600, 600);
-        frame.setLocation(200, 100);
+        frame.setLocation(300, 100);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
