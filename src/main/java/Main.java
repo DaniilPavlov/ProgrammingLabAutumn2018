7d61a5436cfb164acd962c3579e842193547aac3
DaniilPavlov <cool.sharagin@mail.ru>
2018-09-25 23:36:08 +0400

    Main time

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,16 @@
 package main.java;
 
+import main.java.logic.Reversi;
+
+import javax.swing.JFrame;
+
 public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Reversi");
+        frame.setContentPane(new Reversi());
+        frame.setSize(600, 600);
+        frame.setLocation(200, 100);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 }
